76fa0a3b86efebd63400fcb5f5752a1de9891044
Will Gorham <willgorham@gmail.com>
2016-12-22 00:17:03 -0500

    Enable editor on Posts Page

--- src/admin/enable-editor.php
+++ src/admin/enable-editor.php
@@ -9,3 +9,16 @@
  * @license  GNU General Public License 2.0+
  */
 namespace BlogIntro;
+
+add_action( 'edit_form_after_title', __NAMESPACE__ . '\enable_posts_page_editor' );
+
+/**
+ * Enable editor on Posts Page to allow for content entry
+ * @since  1.0.0
+ * @param  \WP_Post $post Current post object
+ */
+function enable_posts_page_editor( $post ) {
+  if ( get_option( 'page_for_posts' ) == $post->ID ) {
+    add_post_type_support( 'page', 'editor' );
+  }
+}
